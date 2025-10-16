273d4bd6b808747dec8a7e84a3859e5d734ec934
nurse-test <mia.mutz@geauxnurseco.com>
2025-10-16 10:05:48 -0700

    Deprecate old environment test function
    The previous implementation of the environment test function has been deprecated.

--- supabase/functions/env-test/index.ts
+++ supabase/functions/env-test/index.ts
@@ -1,14 +1,1 @@
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-
-serve(() => {
-  return new Response(
-    JSON.stringify({
-      PRIVATE_SUPABASE_URL: !!Deno.env.get("PRIVATE_SUPABASE_URL"),
-      PRIVATE_SUPABASE_SERVICE_ROLE_KEY: !!Deno.env.get("PRIVATE_SUPABASE_SERVICE_ROLE_KEY"),
-      STRIPE_SECRET_KEY: !!Deno.env.get("STRIPE_SECRET_KEY"),
-      RESEND_API_KEY: !!Deno.env.get("RESEND_API_KEY"),
-      CLAUDE_API_KEY: !!Deno.env.get("CLAUDE_API_KEY"),
-    }),
-    { headers: { "Content-Type": "application/json" } }
-  );
-});
+ //DEPRECATED go to supabase/functions/env-test.ts
